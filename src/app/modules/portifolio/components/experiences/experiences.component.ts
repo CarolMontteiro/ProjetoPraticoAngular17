0fd0e562c73e87b38295a5a55da9e026a063b596
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-08-14 17:14:15 -0300

    html - Experiences Component

---
+++ src/app/modules/portifolio/components/experiences/experiences.component.ts
@@ -0,0 +1,21 @@
+import { Component, signal } from '@angular/core';
+import { IExperiences } from '../../interface/iExperiences.interface';
+
+@Component({
+  selector: 'app-experiences',
+  standalone: true,
+  imports: [],
+  templateUrl: './experiences.component.html',
+  styleUrl: './experiences.component.scss'
+})
+export class ExperiencesComponent {
+  public arrayExperiences = signal<IExperiences[]>([
+    {
+      summary: {
+        strong:'Lorem Ipsum',
+        p: 'Lorem | Ipsum',
+      },
+       text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In dui turpis, pharetra maximus eros fringilla, facilisis volutpat augue. Nulla bibendum vel mi a elementum',
+    },
+  ]);
+}
