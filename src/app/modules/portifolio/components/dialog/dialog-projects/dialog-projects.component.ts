a4337054df83c89f1cc32e66e56692c179dcc184
CarolMontteiro <carolmonteiro1206@gmail.com>
2025-09-07 00:26:35 -0300

    [HTML + TS] Dialog Component - Adicionando interação do modal e evento de fechar modal

--- src/app/modules/portifolio/components/dialog/dialog-projects/dialog-projects.component.ts
+++ src/app/modules/portifolio/components/dialog/dialog-projects/dialog-projects.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Inject, OnInit, signal } from '@angular/core';
+import { iProjects } from '../../../interface/iProjects.interface';
+import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-dialog-projects',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule],
   templateUrl: './dialog-projects.component.html',
   styleUrl: './dialog-projects.component.scss'
 })
-export class DialogProjectsComponent {
+export class DialogProjectsComponent implements OnInit {
 
+  constructor(
+    private _dialogHef: MatDialogRef<DialogProjectsComponent>,
+    @Inject(MAT_DIALOG_DATA) private _data: iProjects
+  ){}
+
+  public getData = signal<iProjects | null>(null);
+
+  ngOnInit(): void {
+    this.getData.set(this._data);
+  }
+
+  public closeModal() {
+    return this._dialogHef.close();
+  }
 }
